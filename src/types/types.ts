3f598cafaf9f15d406de901c99aa9a7f858d3654
Alex Glau <glaualex89@gmail.com>
2023-04-05 14:48:12 +0400

    Model types

---
+++ src/types/types.ts
@@ -0,0 +1,12 @@
+export interface ICurrentWord {
+  word: string;
+  options: string[];
+  numberOfErrors: number;
+}
+
+export interface ITraining {
+  wordsInTraining: number;
+  words: ICurrentWord[];
+  indexOfTraining?: number;
+  answer?: string[];
+}
