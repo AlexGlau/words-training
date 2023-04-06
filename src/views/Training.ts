e9472b97c8fd2bf37121a5654a53ada55ce741fb
Alex Glau <glaualex89@gmail.com>
2023-04-06 12:09:07 +0400

    Put answer letter to answers' container

--- src/views/Training.ts
+++ src/views/Training.ts
@@ -1,24 +1,32 @@
 import { Button } from "./Button";
+import { Answer } from "./Answer";
 import { controller } from "../controllers/Controller";
 
 export const training = {
   init(): void {
     this.letters = document.getElementById("letters");
+    this.answer = document.getElementById("answer");
     this.render();
   },
   render(): void {
     this.letters.innerHTML = "";
+    this.answer.innerHTML = "";
+
     const words = controller.getCurrentTraining();
 
     if (this.letters) {
       words.options.forEach((letter): void => {
         this.letters.appendChild(
-          new Button(letter, this.handleClick).render()
+          new Button(letter, this.handleClick.bind(this)).render()
         );
       });
     }
   },
-  handleClick(): void {
-    console.log("click");
+  handleClick(e: MouseEvent): void {
+    const letter = (e.target as HTMLButtonElement).innerHTML;
+
+    this.answer.appendChild(
+      new Answer(letter).render()
+    )
   }
 };
