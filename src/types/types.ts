9b77a61e10c9ecc0c9b6461ea1d627138dc30d5a
Alex Glau <glaualex89@gmail.com>
2023-04-08 20:40:08 +0400

    Updated types

--- src/types/types.ts
+++ src/types/types.ts
@@ -4,9 +4,30 @@ export interface ICurrentWord {
   numberOfErrors: number;
 }
 
-export interface ITraining {
+export interface IModel {
   wordsInTraining: number;
   words: ICurrentWord[];
-  indexOfTraining: number;
   answer: string[];
+  indexOfTraining: number;
+  bindOnRender(cb: () => void): void;
+  getCurrentTraining(): ICurrentWord;
+  reduceButtons(letter: string): void;
+}
+
+export type El = HTMLElement | null;
+
+export interface ITraining {
+  clickCount: number;
+  // isCorrect: boolean;
+  // letters: El;
+  // answer: El;
+  // currentQuestion: El;
+  // init(): void;
+  onAnswer(handler: (s: string) => void): void;
+  renderNumberOfWords(value: string): void;
+  render(word: ICurrentWord): void;
+  // updateNumberOfCurrentQuestion(): void;
+  // renderStat(): void;
+  // clearAnswer(): void;
+  // handleClick(e: MouseEvent): void;
 }
